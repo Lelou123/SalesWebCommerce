b3e69a9440bf3b762cd34ceb20f1390f78a4c7cc
Lelou123 <murillosama1@hotmail.com>
2022-03-01 11:54:22 -0300

    Add Seler form viewModel and Department select component

---
+++ SalesWebProject/Services/DepartmentService.cs
@@ -0,0 +1,22 @@
+﻿using SalesWebProject.Data;
+using System.Linq;
+using System.Collections.Generic;
+using SalesWebProject.Models;
+
+namespace SalesWebProject.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebProjectContext _context;
+
+        public DepartmentService(SalesWebProjectContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
--- SalesWebProject/Services/SellerService.cs
+++ SalesWebProject/Services/SellerService.cs
@@ -20,7 +20,7 @@ namespace SalesWebProject.Services
         }
         public void Insert(Seller obj)
         {
-            obj.Department = _context.Department.First();
+            
             _context.Add(obj);
             _context.SaveChanges();
         }
